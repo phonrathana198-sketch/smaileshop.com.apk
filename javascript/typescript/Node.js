398690f6b19b89a83da24639959fa3d2f6aa2b3c
phonrathana198-sketch <phonrathana198@gmail.com>
2025-10-28 01:48:58 +0700

    Create Node.js
    Signed-off-by: phonrathana198-sketch <phonrathana198@gmail.com>

---
+++ javascript/typescript/Node.js
@@ -0,0 +1,41 @@
+import { SocksClient, SocksClientOptions } from 'socks';
+
+const options: SocksClientOptions = {
+  proxy: {
+    host: '192.168.2.1', // Proxy IP
+    port: 443,           // Proxy Port
+    type: 5               // SOCKS5
+  },
+  destination: {
+    host: 'ip-api.com',   // Destination hostname
+    port: 80              // HTTP port
+  },
+  command: 'connect'
+};
+
+SocksClient.createConnection(options)
+  .then(info => {
+    console.log('✅ Connected via SOCKS5 proxy!');
+    const socket = info.socket;
+
+    // ✅ សរសេរ HTTP request ត្រឹមត្រូវ
+    const httpRequest = 
+      'GET /json HTTP/1.1\r\n' +
+      'Host: ip-api.com\r\n' +
+      'Accept: application/json\r\n' +
+      'Connection: close\r\n\r\n';
+
+    socket.write(httpRequest);
+
+    socket.on('data', (data) => {
+      console.log(data.toString());
+    });
+
+    socket.on('end', () => {
+      console.log('🔚 Connection closed');
+    });
+
+  })
+  .catch(err => {
+    console.error('❌ Connection error:', err);
+  });
