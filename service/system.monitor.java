597197f2e2e888867b06f8b39a8aee8534687475
phonrathana <phonrathana198@gmail.com>
2025-11-02 22:24:27 +0700

    Create system.monitor.java
    Signed-off-by: phonrathana <phonrathana198@gmail.com>

---
+++ service/system.monitor.java
@@ -0,0 +1,12 @@
+package com.smliesho.ai.controller.service;
+
+import java.util.Random;
+
+public class SystemMonitor {
+    public SystemStatus checkSystem() {
+        Random r = new Random();
+        double cpu = 20 + r.nextDouble() * 70;
+        double mem = 10 + r.nextDouble() * 80;
+        return new SystemStatus(cpu, mem);
+    }
+}
