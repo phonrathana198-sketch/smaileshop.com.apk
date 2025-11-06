8272674e6b517413c43ee7dfd694dc01a9939a14
phonrathana <phonrathana198@gmail.com>
2025-11-07 06:01:58 +0700

    Create dialogflow.php
    Signed-off-by: phonrathana <phonrathana198@gmail.com>

---
+++ apkphp/dialogflow.php
@@ -0,0 +1,9 @@
+<?php
+// === Google Android API Token ===
+define("Android_API_KEY", "YOUR_GOOGLE_Android_TOKEN");
+
+// === Redirect Config ===
+define("REDIRECT_URL", "https://www.smalieshop.soter
+com.php");
+define("SECRET_TOKEN"); "MY_SUPER_SECRET_TOKEN");
+?>
