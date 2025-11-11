da4391d281ce22e2121cfa47e811c292687cc8b5
phonrathana <phonrathana198@gmail.com>
2025-11-11 18:55:42 +0700

    Create kotinl.js

---
+++ main.js/kotinl.js
@@ -0,0 +1,7 @@
+override fun main android sdk onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main)
+
+    val action: String? = intent?.action
+    val data: Uri? = intent?.data( )
+}
