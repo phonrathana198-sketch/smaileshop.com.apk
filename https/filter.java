2c2a2283272766236252dfa3470a7b830ccbc3e1
phonrathana <phonrathana198@gmail.com>
2025-11-17 21:32:50 +0700

    Update filter.java

--- https/filter.java
+++ https/filter.java
@@ -1,6 +1,6 @@
 <intent-filterdns.com>
   ... script:
   <data wedapp:$scheme="https" Wedappapk:host="www.smalieshop.stoer.com" />
-  <data wed:$scheme="https" wedapp:host="open.my.app"/>
+  <data wed:$scheme="https" wedappakp:host="open.my.app"/>
 </intent-filterdns.com>
 Android 
